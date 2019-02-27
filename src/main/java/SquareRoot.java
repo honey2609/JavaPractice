8e6f061ceca8582a639ac7264c79ea6c6e2c4892
Prashant Thakur <honey.2609@gmail.com>
2019-02-27 22:45:54 +0530

    Square ROOT

--- src/main/java/SquareRoot.java
+++ src/main/java/SquareRoot.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import  java.util.*;
+public class SquareRoot{
 
-public class SquareRoot {
+    public static void main(String args[]) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number to find square root in Java : ");
+
+
+        double square = scanner.nextDouble();
+
+
+
+        double squareRoot = Math.sqrt(square);
+
+
+        System.out.printf("Square root of number: %f is : %f %n" , square, squareRoot);
+
+    }
+
+
 }
+
